1250c3b242e3bfae2bda711fda36ed097cf9f339
Prem Sichanugrist <s@sikac.hu>
2013-04-07 12:44:07 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'pg'
+gem 'jquery-rails'
+gem 'paperclip'
+gem 'paperclip-storage-ftp'
+gem 'twitter-bootstrap-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test, :development do
+  gem 'rspec-rails', '~> 2.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Megumi::Application.config.secret_token = '704cdded663d41d2e61363e435aeb49ec0b8cb7ac0c053adfe235f2b94cd1f611ada24910ddb34879af5d40f7305db6633cd4afc365eef4edfaf1005819ef2fe'
