32929f946680701793b0f42bd03bc74a720f6ada
Prem Sichanugrist <s@sikac.hu>
2013-04-07 21:44:50 -0400

    Show current season name

--- spec/features/user_views_homepage_spec.rb
+++ spec/features/user_views_homepage_spec.rb
@@ -4,6 +4,7 @@ feature 'User views homepage' do
   scenario 'No show available' do
     visit '/'
 
+    page.should have_content('Winter 2013')
     page.should have_content("We currently don't have any screenshot for any show in this season.")
   end
 end
