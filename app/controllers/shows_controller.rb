5272bbd4eb06607604589212062c2110d9cff978
Prem Sichanugrist <s@sikac.hu>
2013-04-07 21:26:16 -0400

    Shows page with no screenshot

---
+++ app/controllers/shows_controller.rb
@@ -0,0 +1,4 @@
+class ShowsController < ApplicationController
+  def index
+  end
+end
---
+++ spec/features/user_views_homepage_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature 'User views homepage' do
+  scenario 'No show available' do
+    visit '/'
+
+    page.should have_content("We currently don't have any screenshot for any show in this season.")
+  end
+end
